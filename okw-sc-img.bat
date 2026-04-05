4334f1683a5051f2c7e425e0a96caf12e1b3757d
y-higa-okf <y-higa@okf.co.jp>
2026-04-06 08:11:14 +0900

    "2026/04/06_08:09:45"

--- okw-sc-img.bat
+++ okw-sc-img.bat
@@ -1,5 +1,3 @@
-REM cmd.exe /c "cd /d C:\Users\okw-y-higa\project\okw-sc-img && git pull origin main --no-rebase && git add . && git commit -m '2026-03-06' && git push origin main"
-
 @echo off
 cd /d C:\Users\okw-y-higa\project\okw-sc-img
 
